5ce36452657edbf0cec1230e0bf67d1bac8bb5d9
Marlo Eichenberg Motta <devmaster@programmerhero.com.br>
2021-08-26 01:07:20 -0300

    feat: Criar uma nova task com um id random, não permita criar caso o título seja vazio.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { FiCheckSquare, FiTrash } from 'react-icons/fi';
+import '../styles/tasklist.scss';
 
-import '../styles/tasklist.scss'
 
-import { FiTrash, FiCheckSquare } from 'react-icons/fi'
 
 interface Task {
   id: number;
@@ -16,6 +16,13 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle) {
+      setTasks(prevTasks => [...prevTasks, {
+        id: Date.now(),
+        title: newTaskTitle,
+        isComplete: false
+      }]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
